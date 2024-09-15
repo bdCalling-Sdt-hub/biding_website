e0b5f42b0f962f566cb12f563628f64511268486
shukumar-542 <shukumar542@gmail.com>
2024-09-15 17:18:15 +0600

    Initialized biding website project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MainLayout from './components/layouts/MainLayout'
+
+function App() {
+
+  return (
+    <div>
+      <MainLayout/>
+    </div>
+  )
+}
+
+export default App
