152f5d1c7156d5f02f85fd2ff8cb74012a87c091
shukumar-542 <shukumar542@gmail.com>
2024-09-16 18:19:30 +0600

    Implement responsive for mobile and tab device

--- src/pages/Notification/Notification.jsx
+++ src/pages/Notification/Notification.jsx
@@ -13,21 +13,28 @@ const Notification = () => {
                 <Link to={-1}><IoArrowBackSharp className='text-yellow' /></Link>
                 <p>Home / Notification</p>
             </div>
-            <div className='grid grid-cols-12  justify-between gap-5'>
-                <div className='col-span-10 bg-white rounded-lg m-1 p-4'>
-                    <h1 className='text-[24px] font-medium pb-4'>Notification</h1>
-                    {
-                        product?.map(product => <NotificationCard />)
-                    }
-                </div>
-                <div className='col-span-2 '>
-                    <p className='font-medium text-[18px] pb-5'>Upcoming Auction:</p>
-                    {
-                        product?.map(product => <UpcommingProduct />)
-                    }
+         
 
+
+            <div className='grid grid-cols-12 justify-between gap-5'>
+                <div className='col-span-12 lg:col-span-10 bg-white rounded-lg p-4'>
+                    <h1 className='text-[24px] font-medium pb-4'>Notification</h1>
+                    {product?.map((product, index) => (
+                        <NotificationCard key={index} />
+                    ))}
+                </div>
+
+                <div className='col-span-12 lg:col-span-2 px-2 lg:px-0'>
+                    <p className='font-medium text-[18px] pb-5'>Upcoming Auction:</p>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
+                        {product?.map((product, index) => (
+                            <UpcommingProduct key={index} />
+                        ))}
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
