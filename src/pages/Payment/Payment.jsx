c1ea7c9684f165190ea3451845163f19bdb91b51
shukumar-542 <shukumar542@gmail.com>
2024-09-18 12:25:32 +0600

    Impelment payment page change address modal

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BackButton from '../../components/ui/BackButton'
 import img from '../../assets/phone1.png'
-import { Form, Input, Tabs } from 'antd'
-import CheckoutFormStripe from '../../components/ui/PaymentStripe'
+import { Form, Input, Modal, Tabs } from 'antd'
 import PaymentStripe from '../../components/ui/PaymentStripe'
 import PaymentPayPal from '../../components/ui/PaymentPayPal'
-
+import Button from '../../components/ui/Button'
 const Payment = () => {
-  
+    const [modalOpen, setModalOpen] = useState(false);
 
     const items = [
         {
             key: '1',
             label: 'Credit Card',
-            children: <PaymentStripe/>,
+            children: <PaymentStripe />,
         },
         {
             key: '2',
             label: 'PayPal',
-            children: <PaymentPayPal/>,
+            children: <PaymentPayPal />,
         },
         ,
     ];
+
+
+    /**Handle change location  */
+    const onFinish = (values) => {
+        console.log(values);
+    }
+
     return (
         <div className='px-5 lg:px-0'>
             <BackButton pageName={'Payment'} />
@@ -55,7 +61,7 @@ const Payment = () => {
                         <p>Zip Code:<span className='font-medium'> 62957</span></p>
                         <p>Phone Number: <span className='font-medium'> (406) 555-0120</span></p>
                         <div className='pt-5'>
-                            <button className='text-yellow border  rounded-lg w-[70%] py-1'>Change Shipping Address</button>
+                            <button onClick={() => setModalOpen(true)} className='text-yellow border  rounded-lg w-[70%] py-1'>Change Shipping Address</button>
                         </div>
                     </div>
                     <div className='bg-white p-8 rounded-md'>
@@ -63,6 +69,95 @@ const Payment = () => {
                     </div>
                 </div>
             </div>
+            <Modal
+                title="Change Shipping Address"
+                centered
+                open={modalOpen}
+                onCancel={() => setModalOpen(false)}
+                footer={false}
+            >
+                <Form
+                    onFinish={onFinish}
+                    layout="vertical"
+
+                >
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[
+                            {
+                                message: 'Please enter your name!',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter your full name" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Street Address"
+                        name="address"
+                        rules={[
+                            {
+                                message: 'Please enter street address',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter your street address" />
+                    </Form.Item>
+                    <Form.Item
+                        label="City"
+                        name="city"
+                        rules={[
+                            {
+                                message: 'Please enter city',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter City" />
+                    </Form.Item>
+                    <Form.Item
+                        label="State"
+                        name="state"
+                        rules={[
+                            {
+                                message: 'Please enter state',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter state" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Zip Code"
+                        name="zip"
+                        rules={[
+                            {
+                                message: 'Please enter Zip code',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter Zip code" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        rules={[
+                            {
+                                message: 'Please enter phone number',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Enter Phone Number" />
+                    </Form.Item>
+                    <div className='flex  items-center justify-center w-full gap-5 '>
+                        <Form.Item>
+
+                            <button onClick={()=> setModalOpen(false)} className='border border-yellow rounded-md px-10 py-1 text-yellow'>cancel</button>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button className='w-full px-10' >Submit</Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+            </Modal>
         </div>
     )
 }
