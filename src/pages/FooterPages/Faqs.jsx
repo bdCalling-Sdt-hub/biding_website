0d9dfe2865362abe82d93e06145a732dff71d7f5
shukumar-542 <shukumar542@gmail.com>
2024-09-30 16:19:15 +0600

    Implement FAQ api integration

--- src/pages/FooterPages/Faqs.jsx
+++ src/pages/FooterPages/Faqs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BackButton from '../../components/ui/BackButton'
 import { Collapse } from 'antd'
+import { useGetFaqQuery } from '../../redux/api/manageApis';
 const { Panel } = Collapse;
 
 
@@ -48,17 +49,14 @@ const panels = [
     },
 ];
 const Faqs = () => {
+        const {data : getFaqs} = useGetFaqQuery()
+        const middleIndex = Math.ceil((getFaqs?.data?.length || 0) / 2);
+        const firstHalf = getFaqs?.data?.slice(0, middleIndex);
+        const secondHalf = getFaqs?.data?.slice(middleIndex);
 
-
-    const firstHalf = panels.slice(0, 4);
-    const secondHalf = panels.slice(4, 8);
-
-    // const middleIndex = Math.ceil(panels.length / 2);
-    // const firstHalf = panels.slice(0, middleIndex);
-    // const secondHalf = panels.slice(middleIndex);
     return (
         <div className='px-5 md:px-0'>
-            <BackButton pageName={'FAQs'} />
+            <BackButton pageName={'FAQs'}/>
             {/* FAQs section */}
             <div>
                 <h1 className='mb-2 font-semibold'>FAQs: </h1>
@@ -67,18 +65,18 @@ const Faqs = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 gap-5 py-5'>
                 <div>
                     <Collapse>
-                        {firstHalf.map(panel => (
-                            <Panel key={panel.key} header={panel.label}  className='panel-margin space-y-5'>
-                            {panel.children}
+                        {firstHalf?.map(panel => (
+                            <Panel key={panel.key} header={panel.question}  className='panel-margin space-y-5'>
+                            {panel.answer}
                         </Panel>
                         ))}
                     </Collapse>
                 </div>
                 <div>
                     <Collapse>
-                        {secondHalf.map(panel => (
-                            <Panel key={panel.key} header={panel.label}>
-                                {panel.children}
+                        {secondHalf?.map(panel => (
+                            <Panel key={panel.key} header={panel.question}>
+                                {panel.answer}
                             </Panel>
                         ))}
                     </Collapse>
