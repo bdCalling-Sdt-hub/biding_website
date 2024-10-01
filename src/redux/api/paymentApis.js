d7a61b60478160f7484e0d502c21079dfbe7cba8
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-01 12:28:43 +0600

    fix issyes

--- src/redux/api/paymentApis.js
+++ src/redux/api/paymentApis.js
@@ -4,19 +4,48 @@ const paymentApis = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         //create payment intant
         createPaymentIntent: builder.mutation({
-            query: ({ amount }) => {
-                console.log('amount', amount)
+            query: (data) => {
                 return {
                     url: `/payment/create-payment-intent`,
                     method: 'POST',
-                    body: { amount },
+                    body: data,
                 }
             },
             invalidatesTags: ['payment']
+        }),
+        // confirm payment 
+        confirmPayment: builder.mutation({
+            query: (data) => ({
+                url: '/payment/execute-payment-with-credit',
+                method: 'POST',
+                body: data
+            })
+        }),
+        // paypal confirm payment 
+        paypalConfirmPayment: builder.mutation({
+            query: (data) => ({
+                url: '/payment/execute-payment',
+                method: 'POST',
+                body: data
+            })
+        }),
+        //paypal create payment
+        paypalCreatePayment: builder.mutation({
+            query: (data) => ({
+                url: '/payment/create-payment',
+                method: 'POST',
+                body: data
+            })
         })
     })
 })
 export const {
     // create payment mutation 
-    useCreatePaymentIntentMutation
+    useCreatePaymentIntentMutation,
+    // confirm  payment 
+    useConfirmPaymentMutation,
+    // paypal confirm payment 
+    usePaypalConfirmPaymentMutation,
+    // paypal create payment  
+    usePaypalCreatePaymentMutation
 } = paymentApis
