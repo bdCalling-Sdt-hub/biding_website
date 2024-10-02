fc047f61f9604eafef5419747e2cdd644884bbcc
shukumar-542 <shukumar542@gmail.com>
2024-10-02 17:20:51 +0600

    Implement loading for the home page data

--- src/pages/Home/UpComingAuction.jsx
+++ src/pages/Home/UpComingAuction.jsx
@@ -4,10 +4,11 @@ import { IoIosArrowForward } from 'react-icons/io'
 import { useGetWinnerQuery } from '../../redux/api/winnerApi'
 import UpcommingProduct from '../../components/ui/UpcommingProduct'
 import { Link } from 'react-router-dom'
+import { Spin } from 'antd'
 
 const UpComingAuction = () => {
     const [page, setPage] = useState(1)
-    const { data: upcomingData } = useGetWinnerQuery({ status: "UPCOMING", page })
+    const { data: upcomingData , isLoading } = useGetWinnerQuery({ status: "UPCOMING", page })
     return (
         <div>
             <div className='flex justify-between items-center'>
@@ -15,7 +16,7 @@ const UpComingAuction = () => {
                 <Link to='/upcoming-auction' className='text-yellow flex items-center gap-1 font-medium cursor-pointer'>See More <IoIosArrowForward /></Link>
             </div>
             <div className='grid grid-col-1 md:grid-cols-4 gap-5 my-5 px-2 md:px-0'>
-                {upcomingData?.data?.result?.slice(0, 4)?.map((product, index) => (
+                { isLoading ? <div className='flex items-center justify-center col-span-2 md:col-span-4'><Spin size='large'/></div> :  upcomingData?.data?.result?.slice(0, 4)?.map((product, index) => (
                     <UpcommingProduct key={index} product={product} />
                 ))}
             </div>
